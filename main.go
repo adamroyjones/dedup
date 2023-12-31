78b3ccafe671a47f74cd121ad6cb5971f85a5bfe
Adam Jones <10088591+adamroyjones@users.noreply.github.com>
2023-12-31 20:05:04 +0000

    style: Update the error-handling to use %w, lowercased error strings, etc.
    See ST1005: https://staticcheck.dev/docs/checks/#ST1005

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -44,15 +45,15 @@ func dedup(args []string, write bool) error {
 	for _, line := range dedupedLines {
 		if _, err := out.WriteString(line + "\n"); err != nil {
 			if closeErr := out.Close(); closeErr != nil {
-				return fmt.Errorf("Error when writing the deduplicated lines: %v; error when closing the destination: %v", err, closeErr)
+				return fmt.Errorf("writing the deduplicated lines: %w; error when closing the destination: %w", err, closeErr)
 			}
-			return fmt.Errorf("Error when writing the deduplicated lines: %v", err)
+			return fmt.Errorf("writing the deduplicated lines: %w", err)
 		}
 	}
 
 	if write {
 		if err := os.Rename(out.Name(), in.Name()); err != nil {
-			return fmt.Errorf("Error when writing the deduplicated file: %v", err)
+			return fmt.Errorf("writing the deduplicated file: %w", err)
 		}
 	}
 
@@ -61,42 +62,51 @@ func dedup(args []string, write bool) error {
 
 func preparePipes(args []string, write bool) (*os.File, *os.File, error) {
 	switch len(args) {
-	case 1: // Deduplicate STDIN and print the results to STDOUT.
+	case 0:
+		panic("unreachable")
+
+	case 1:
+		// Deduplicate STDIN and print the results to STDOUT.
 		return os.Stdin, os.Stdout, nil
-	case 2: // Deduplicate a file and print the results to STDOUT.
+
+	case 2:
+		// Deduplicate a file and print the results to STDOUT.
 		if write {
-			return nil, nil, fmt.Errorf("Given the '-w' flag, but not given a corresponding file to overwrite")
+			return nil, nil, errors.New("given the -w flag, but not given a corresponding file to overwrite")
 		}
 
 		in, err := os.Open(os.Args[1])
 		if err != nil {
-			return nil, nil, fmt.Errorf("Unable to open file '%s' for reading: %v", os.Args[1], err)
+			return nil, nil, fmt.Errorf("unable to open %q for reading: %w", os.Args[1], err)
 		}
 
 		return in, os.Stdout, nil
-	case 3: // Deduplicate a file in-place.
+
+	case 3:
+		// Deduplicate a file in-place.
 		if !write {
-			// TODO: This could be generalised, I suppose, but it's not fruitful given that you can already cat the files.
-			return nil, nil, fmt.Errorf("Given more than one file")
+			return nil, nil, errors.New("given more than one file: use cat, if necessary")
 		}
 
 		if os.Args[1] != "-w" {
-			return nil, nil, fmt.Errorf("The '-w' flag should be given as the first argument")
+			return nil, nil, errors.New("the -w flag must be given as the first argument")
 		}
 
 		in, err := os.Open(os.Args[2])
 		if err != nil {
-			return nil, nil, fmt.Errorf("Unable to open file '%s' for reading: %v", os.Args[1], err)
+			return nil, nil, fmt.Errorf("unable to open file %q for reading: %w", os.Args[1], err)
 		}
 
 		out, err := os.CreateTemp(os.TempDir(), "dedup-")
 		if err != nil {
-			return nil, nil, fmt.Errorf("Unable to create a temporary file for deduplication: %v", err)
+			return nil, nil, fmt.Errorf("unable to create a temporary file for deduplication: %w", err)
 		}
 
 		return in, out, nil
-	default: // Given 4 or more arguments, which is an error case.
-		return nil, nil, fmt.Errorf("Unexpectedly given more than 3 arguments")
+
+	default:
+		// Given 4 or more arguments. This is an error case.
+		return nil, nil, errors.New("unexpectedly given more than 3 arguments")
 	}
 }
 
